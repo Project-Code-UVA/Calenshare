45723d11dfdf73d2ab354099994d4a5e686a4c2d
Arthur Wu <arthur.kainan.wu@gmail.com>
2023-10-15 01:48:31 -0400

    added tasks

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" style="width: 200px;">
   <loginPage/>
+  <calendarCreate/>
   <!--
+    Source:
     <a href="https://www.freepik.com/free-vector/schedule-calendar-flat-style_41411895.htm#query=calendar&position=7&from_view=keyword&track=sph">Image by juicy_fish</a> on Freepik
   -->
 </template>
 
 <script>
 import loginPage from './components/loginPage.vue'
+import calendarCreate from './components/calendarCreate.vue'
 
 export default {
   name: 'App',
   components: {
-    loginPage
+    loginPage,
+    calendarCreate
   }
 }
 </script>
