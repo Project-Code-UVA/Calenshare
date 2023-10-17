ba87498018473acf8ce76602c25d06bf189d3334
BGV <26331505+ed789d0@users.noreply.github.com>
2023-10-16 20:37:15 -0400

    Separated styles into separate file

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
 <script>
 import loginPage from './components/loginPage.vue'
 import calendarCreate from './components/calendarCreate.vue'
+import './assets/style.css'
 
 export default {
   name: 'App',
@@ -20,14 +21,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
