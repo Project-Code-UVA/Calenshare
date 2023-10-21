437a4b3095bc6f242b9297e1c6ab4a543321bf72
BGV <26331505+ed789d0@users.noreply.github.com>
2023-10-21 16:28:42 -0400

    HTML tweaks

--- src/components/loginPage.vue
+++ src/components/loginPage.vue
@@ -3,11 +3,11 @@
     <h1>Welcome to Calenshare</h1>
     
     <form>
-      <input placeholder="Enter link here">
-      <button type="submit" id="submit-link" aria-label="Open this link" title="Open this link">➔</button>
+      <input class="left-side" placeholder="Enter link here"> <!-- TODO: add screen-reader friendly placeholder -->
+      <button type="submit" id="submit-link" class="right-side" aria-label="Open this link" title="Open this link">➔</button>
     </form>
     <p class="or-separator"><i>———— or ————</i></p>
-    <button class="primary"><span aria-hidden>🔗</span> Create new link</button>
+    <button class="primary"><span aria-hidden>🔗</span> Create new link</button> 
   </div>
 </template>
 
