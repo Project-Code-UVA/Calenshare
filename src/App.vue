97b53d97e18f451eb2d063a3f3aa157d28dbdcaa
BGV <26331505+ed789d0@users.noreply.github.com>
2023-10-22 22:31:00 -0400

    changed div to nav

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <nav>
     <router-link to="/">Home</router-link> | 
     <router-link to="/create">Create</router-link>
-    <router-view></router-view>
-  </div>
+  </nav>
+  <router-view></router-view>
 </template>
 
 <script>
