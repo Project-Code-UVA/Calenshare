ac69402456b839f7987076ec794119d4a3c995ff
Arthur Wu <arthur.kainan.wu@gmail.com>
2023-10-22 23:58:19 -0400

    separated calendarCreate component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" style="width: 200px;">
   <loginPage/>
-  <calendarCreate/>
   <!--
     Source:
     <a href="https://www.freepik.com/free-vector/schedule-calendar-flat-style_41411895.htm#query=calendar&position=7&from_view=keyword&track=sph">Image by juicy_fish</a> on Freepik
@@ -10,13 +9,13 @@
 
 <script>
 import loginPage from './components/loginPage.vue'
-import calendarCreate from './components/calendarCreate.vue'
+
 
 export default {
   name: 'App',
   components: {
     loginPage,
-    calendarCreate
+
   }
 }
 </script>
