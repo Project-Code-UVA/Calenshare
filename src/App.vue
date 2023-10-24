2ac68ed8bbde2aea15c5821e1aaaff5e87e28ec4
Alex Chen <alexchen466@gmail.com>
2023-10-24 00:11:14 -0400

    Added sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <nav>
+  <sidebar/>
+  <nav :style = "{'margin-left': sidebarWidth}">
     <router-link to="/">Home</router-link> | 
     <router-link to="/create">Create</router-link>
   </nav>
-  <router-view></router-view>
+  <div :style = "{'margin-left': sidebarWidth}">
+    <router-view></router-view>
+  </div>
+
 </template>
 
 <script>
 import './assets/style.css'
+import sidebar from '@/components/sidebar/pageSidebar'
+import { sidebarWidth } from './components/sidebar/state';
 
 export default {
   name: 'App',
+  components: {sidebar},
+  setup() {
+    return {sidebarWidth}
+  }
 }
 </script>
